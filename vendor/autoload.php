7e0d92285ddb02b437ed5cd1d63cf2d2afbb3e17
U-RICHARDASUS\Richard <richard.axelsson@gmail.com>
2021-06-02 01:51:11 +0200

    Final version, minor change in db

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitc24077715b6f621c42b300aaa371b3ed::getLoader();
+return ComposerAutoloaderInit316931ad1ce90fe0a7154471664c7f22::getLoader();
